9bc48eadaac5869234c7518caa0ee4d9497a9300
Jesse Liu <neoliu2011@gmail.com>
2026-09-05 15:41:56 +0100

    synchronize the post patch of llvm to icpp's.

--- build.cc
+++ build.cc
@@ -17,15 +17,13 @@ Usage:
 
 #include <icpp.hpp>
 
-namespace fs = std::filesystem;
-
 #if 1 // change to 0 to only print the command without executing it.
 #define command(fmt, ...) std::system(std::format(fmt, __VA_ARGS__).c_str())
 #else
 #define command(fmt, ...) std::println(fmt, __VA_ARGS__);
 #endif
 
-#if _WIN32
+#if __WIN__
 #define EXTRA_CMAKE                                                            \
   " -DCMAKE_C_COMPILER=clang-cl -DCMAKE_CXX_COMPILER=clang-cl "
 
@@ -69,9 +67,33 @@ bool patch_string(std::string_view infile, std::string_view pattern,
 // AetherBinary links to LLVM.dll, the LLVM_ABI will cause problem
 // on Windows with linkage issues, so we remove its LLVM_ABI attribute
 // before building or after installing...
-void patch_file(std::string_view file) {
+void patch_llvm_abi(std::string_view file) {
   std::println("Patching LLVM_ABI declaration in {}...", file);
   if (patch_string(file, " LLVM_ABI ", " /*LLVM_ABI*/ "))
+    std::println("Finished patching.");
+  else
+    std::println("Ignored, it's already been patched.");
+}
+
+void patch_undef_llvm_abi(std::string_view file) {
+  std::println("Patching LLVM_ABI declaration in {}...", file);
+  if (patch_string(file, "#if !defined(LLVM_ABI)",
+                   R"(
+#undef LLVM_ABI
+#undef LLVM_TEMPLATE_ABI
+#define LLVM_ABI
+#define LLVM_TEMPLATE_ABI
+
+#if !defined(LLVM_ABI))"))
+    std::println("Finished patching.");
+  else
+    std::println("Ignored, it's already been patched.");
+}
+
+void patch_error_id(std::string_view file) {
+  std::println("Patching LLVM_ABI declaration in {}...", file);
+  if (patch_string(file, "static char ID;",
+                   "__declspec(dllimport) static char ID;"))
     std::println("Finished patching.");
   else
     std::println("Ignored, it's already been patched.");
@@ -97,6 +119,15 @@ int main(int argc, const char *argv[]) {
   auto script_path = fs::absolute(argv[0]);
   std::println("Running build script {}...", script_path.string());
 
+  // set the library path of icpp's c++ runtime, so that llvm's table-gen can
+  // run normally
+#if __WIN__
+  icpp::set_env(
+      "PATH", std::format("{}/bin;{}", icpp_dir.string(), std::getenv("PATH")));
+#elif __LINUX__
+  icpp::set_env("LD_LIBRARY_PATH",
+                std::format("{}/lib", icpp_dir.string()).c_str());
+#endif
   auto script_dir = script_path.parent_path();
 
   std::println("Phase 1: Build LLVM...");
@@ -107,40 +138,25 @@ int main(int argc, const char *argv[]) {
   if (fs::exists(llvm_install_dir)) {
     std::println("LLVM has already been built and installed.");
   } else {
-#if _WIN32
+#if __WIN__
     // pre-build patch
-    patch_file(script_dir.string() + "/third/llvm-project/llvm/include/llvm/"
-                                     "ExecutionEngine/Orc/BacktraceTools.h");
+    patch_llvm_abi(script_dir.string() +
+                   "/third/llvm-project/llvm/include/llvm/"
+                   "ExecutionEngine/Orc/BacktraceTools.h");
 #endif
 
     command("cmake -S {} -B {} -G Ninja -DICPP_INSTALL_DIR={} {}",
             dqpath(llvm_cmake_dir), dqpath(llvm_build_dir), dqpath(icpp_dir),
             EXTRA_CMAKE);
 
-    // link icpp's c++ runtime libraries, so that llvm's table-gen can run
-    // normally
-#if _WIN32
-    auto dstcpp = llvm_build_dir / "llvm/bin/c++.dll";
-    if (!fs::exists(dstcpp)) {
-      std::println("Created symlink {}.", dstcpp.string());
-      command("mklink {} {}", dqpath(dstcpp), dqpath(icpp_dir / "lib/c++.dll"));
-    }
-#elif __linux__
-    for (auto lib : {"c++", "c++abi", "unwind"}) {
-      auto name = std::format({"lib{}.so.1"}, lib);
-      auto dst = llvm_build_dir / "llvm/lib" / name;
-      if (fs::exists(dst))
-        break;
-      std::println("Created symlink {}.", dst.string());
-      command("ln -s {} {}", dqpath(icpp_dir / "lib" / name), dqpath(dst));
-    }
-#endif
-
     command("cmake --build {} --target install", dqpath(llvm_build_dir));
 
-#if _WIN32
+#if __WIN__
     // post-install patch
-    patch_file(llvm_install_dir.string() + "/include/llvm/ADT/SmallVector.h");
+    patch_error_id(llvm_install_dir.string() + "/include/llvm/Support/Error.h");
+    patch_undef_llvm_abi(llvm_install_dir.string() +
+                         "/include/llvm/Support/Compiler.h");
+
 #endif
   }
 
@@ -149,7 +165,7 @@ int main(int argc, const char *argv[]) {
   auto build_type = "Release";
   if (argc > 1) {
     build_type = argv[1];
-#if _WIN32
+#if __WIN__
     build_type = "RelWithDebInfo";
 #endif
   }
